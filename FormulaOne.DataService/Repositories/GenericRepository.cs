1c01c3da5834964b97f6a0e22a5ee8c5e4feab56
David Nwachukwu <davidnw1998@gmail.com>
2024-07-16 17:56:37 +0100

    implement generic repository

---
+++ FormulaOne.DataService/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using FormulaOne.DataService.Data;
+using FormulaOne.DataService.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FormulaOne.DataService.Repositories;
+
+public class GenericRepository<T>(AppDbContext context, ILogger logger) : IGenericRepository<T>
+    where T : class
+{
+    public readonly ILogger _logger = logger;
+    protected AppDbContext _context = context;
+    internal DbSet<T> _dbSet = context.Set<T>();
+
+    public async Task<bool> Add(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return true;
+    }
+
+    public Task<IEnumerable<T>> All()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T?> GetById(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public Task<bool> Update(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
