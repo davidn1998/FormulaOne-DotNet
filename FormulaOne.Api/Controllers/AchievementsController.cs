e4f6d8d1c1b94615b51b76b3e39d55f36747d650
David Nwachukwu <davidnw1998@gmail.com>
2024-07-16 21:41:52 +0100

    add getdriverachievements route

--- FormulaOne.Api/Controllers/AchievementsController.cs
+++ FormulaOne.Api/Controllers/AchievementsController.cs
@@ -1,7 +1,26 @@
 ﻿using AutoMapper;
 using FormulaOne.DataService.Repositories.Interfaces;
+using FormulaOne.Entities.Dtos.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FormulaOne.Api.Controllers;
 
 public class AchievementsController(IUnitOfWork unitOfWork, IMapper mapper)
-    : BaseController(unitOfWork, mapper) { }
+    : BaseController(unitOfWork, mapper)
+{
+    [HttpGet]
+    [Route("{driverId:guid}")]
+    public async Task<IActionResult> GetDriverAchievements(Guid driverId)
+    {
+        var driverAchievements = await _unitOfWork.Achievements.GetDriverAchievementAsync(driverId);
+
+        if (driverAchievements is null)
+        {
+            return NotFound("Achievements not found");
+        }
+
+        var result = _mapper.Map<DriverAchievementResponse>(driverAchievements);
+
+        return Ok(result);
+    }
+}
---
+++ FormulaOne.Entities/Dtos/Responses/DriverAchievementResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace FormulaOne.Entities.Dtos.Responses;
+
+public class DriverAchievementResponse
+{
+    public Guid DriverId { get; set; }
+    public int WorldChampionship { get; set; }
+    public int PolePositions { get; set; }
+    public int FastestLaps { get; set; }
+    public int Wins { get; set; }
+}
