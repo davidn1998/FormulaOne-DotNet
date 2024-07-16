5c621a52f244ef8309d5aca34ef19b79a8044771
David Nwachukwu <davidnw1998@gmail.com>
2024-07-16 20:41:38 +0100

    add repo service to api

--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -1,4 +1,6 @@
 using FormulaOne.DataService.Data;
+using FormulaOne.DataService.Repositories;
+using FormulaOne.DataService.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,8 @@ builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
