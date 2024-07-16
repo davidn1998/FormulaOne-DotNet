01f0c06869c9435e5a0b1893cf1ec60024506f6c
David Nwachukwu <davidnw1998@gmail.com>
2024-07-16 13:49:44 +0100

    setup db context

---
+++ FormulaOne.DataService/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FormulaOne.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOne.DataService.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    // define the DbSet for the entities
+    public virtual DbSet<Driver> Drivers { get; set; }
+    public virtual DbSet<Achievement> Achievements { get; set; }
+
+    // define the relationship between the entities
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Achievement>()
+            .HasOne(a => a.Driver)
+            .WithMany(d => d.Achievements)
+            .HasForeignKey(a => a.DriverId)
+            .OnDelete(DeleteBehavior.NoAction)
+            .HasConstraintName("FK_Achievement_Driver");
+    }
+}
